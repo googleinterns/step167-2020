bb913bcb00b878922134c6bbff2fd346b98ed1d2
Narahari Rao <narahari387@gmail.com>
2020-07-24 15:03:02 -0700

    Add Comment and PostMetadata classes

---
+++ src/main/java/com/google/sps/data/PostMetadata.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+public class PostMetadata {
+
+	public static final String TITLE_KEY = "title";
+	public static final String TIMESTAMP_KEY = "timestamp";
+
+	public String uid;
+	public String title;
+	public long timestamp;
+}
