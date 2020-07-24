be6e1b1ee7f408d527b509c2a76e595d70872eda
Narahari Rao <narahari387@gmail.com>
2020-07-24 15:01:20 -0700

    Move DatabaseReferences to DBReferences for differentiation with firebase class DatabaseReference

--- src/main/java/com/google/sps/data/DBReferences.java
+++ src/main/java/com/google/sps/data/DBReferences.java
@@ -3,8 +3,10 @@ package com.google.sps.data;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 
-public class DatabaseReferences {
+public class DBReferences {
 	public static final FirebaseDatabase DB = FirebaseDatabase.getInstance();
 	public static final DatabaseReference ROOT = DB.getReference();
-	public static final DatabaseReference POSTS = DB.getReference("posts");
+	public static final DatabaseReference POST_DATA = DB.getReference("post-data");
+	public static final DatabaseReference POST_METADATA = DB.getReference("post-metadata");
+	public static final DatabaseReference COMMENTS = DB.getReference("comments");
 }
