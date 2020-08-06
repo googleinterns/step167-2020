9c01bb2c0de25db90460fe942464aea443eec81a
Bradley <pickbradley23@gmail.com>
2020-08-06 23:00:56 +0000

    Adjust parameters/return of interface methods

--- src/main/java/com/google/sps/data/DBInterface.java
+++ src/main/java/com/google/sps/data/DBInterface.java
@@ -1,28 +1,29 @@
 package com.google.sps.meltingpot.data;
 
 public interface DBInterface {
-    public String getRecipeContent(String Id);
-    public RecipeMetadata getRecipeMetadata(String Id);
-    public String addRecipe(RecipeMetadata newRecipe, String newContent);
-    public void deleteRecipe(String Id);
-    public void editRecipeTitleContent(String editedTitle, String editedContent);
+  public String getRecipeContent(String Id);
+  public RecipeMetadata getRecipeMetadata(String Id);
+  public String addRecipe(RecipeMetadata newRecipe, String newContent);
+  public void deleteRecipe(String Id);
+  public void editRecipeTitleContent(String editedTitle, String editedContent);
 
-    public Iterable<RecipeMetadata> getAllRecipes();
-    public Iterable<Comment> getAllCommentsInRecipe(String recipeId);
-    public Iterable<Tag> getAllTags();
+  public Iterable<RecipeMetadata> getAllRecipes();
+  public Iterable<Comment> getAllCommentsInRecipe(String recipeId);
+  public Iterable<Tag> getAllTags();
 
-    public User getUser(String userId);
-    public String addUser();
-    public void deleteUser(String userId);
-    public void addRecipeIdToCreated(String userId, String recipeId);
-    public void addRecipeIdToSaved(String userId, String recipeId);
-    public void followTag(String userId, String tagId);
+  public User getUser(String userId);
+  public String addUser();
+  public void deleteUser(String userId);
+  public void addRecipeIdToCreated(String userId, String recipeId);
+  public void addRecipeIdToSaved(String userId, String recipeId);
+  public void followTag(String userId, String tagId);
 
-    public Iterable<RecipeMetadata> getRecipesMatchingTags(Iterable<String> tagIds);
-    public Iterable<RecipeMetadata> getRecipesMatchingCreator(String creatorId);
-    public Iterable<RecipeMetadata> getRecipesMatchingIDs(Iterable<String> Ids);
+  public Iterable<RecipeMetadata> getRecipesMatchingTags(Iterable<String> tagIds);
+  public Iterable<RecipeMetadata> getRecipesMatchingCreator(String creatorId);
+  public Iterable<RecipeMetadata> getRecipesSavedBy(String userId);
+  public Iterable<RecipeMetadata> getRecipesMatchingIDs(Iterable<String> Ids);
 
-    public Comment addComment(Comment newComment);
-    public void deleteComment(String Id, String recipeId);
-    public void editCommentContent(String editedContent);
+  public String addComment(Comment newComment, String recipeId);
+  public void deleteComment(String Id, String recipeId);
+  public void editCommentContent(String Id, String recipeId, String editedContent);
 }
