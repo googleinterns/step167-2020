f099928d31b70ebd28a4e14f6da885e179859613
Narahari Rao <naraharirao@google.com>
2020-08-11 14:23:06 -0700

    Add Tag constructor and toString

--- src/main/java/com/google/sps/data/Tag.java
+++ src/main/java/com/google/sps/data/Tag.java
@@ -15,4 +15,15 @@ public class Tag extends DBObject {
   public Tag(String id) {
     super(id);
   }
+  
+  public Tag(String id, String name, boolean hidden, String type) {
+    super(id);
+    this.name = name;
+    this.hidden = hidden;
+    this.type = type;
+  }
+
+  public String toString() {
+    return "Tag[id=" + super.id + ", name=" + name + ", hidden=" + String.valueOf(hidden) + ", type=" + type + "}";
+  }
 }
