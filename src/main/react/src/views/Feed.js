84e0a76378b607fc3fcbf97dedb50f60adc09e64
Narahari Rao <naraharirao@google.com>
2020-08-26 11:47:39 -0700

    add frontend pagination (preloads next page)

--- src/main/react/src/views/Feed.js
+++ src/main/react/src/views/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import {
   CButton,
@@ -19,18 +19,18 @@ import app from "firebase/app";
 import "firebase/auth";
 import FeedMap from "../components/FeedMap";
 
-const getRecipes = async feedType => {
-  let qs = requestRoute + "api/post";
+const getRecipes = async (feedType, page) => {
+  let qs = requestRoute + "api/post?page=" + page;
   if (feedType === "saved") {
     let token = await app.auth().currentUser.getIdToken();
-    qs += "?saved=true&sort=NEW&token=" + token;
+    qs += "&saved=true&sort=NEW&token=" + token;
   } else if (feedType === "created") {
     let token = await app.auth().currentUser.getIdToken();
-    qs += "?sort=NEW&token=" + token;
+    qs += "&sort=NEW&token=" + token;
   } else if (feedType === "popular") {
-    qs += "?sort=TOP";
-  } else if(feedType === "new") {
-    qs += "?sort=NEW";
+    qs += "&sort=TOP";
+  } else if (feedType === "new") {
+    qs += "&sort=NEW";
   }
   let res = await fetch(qs);
   let data = await res.json();
@@ -41,26 +41,36 @@ const Feed = props => {
   const [recipes, setRecipes] = useState([]);
   const [tags, setTags] = useState({});
 
+  const [page, setPage] = useState(0);
+
   const [errMsg, setErrMsg] = useState("");
 
   const [loaded, setLoaded] = useState(false);
+
+  const loadRecipes = useCallback(
+    async (user, recipePage) => {
+      let recipeData = await getRecipes(props.feedType, recipePage);
+      if (user) {
+        let [voteData, savedData] = await Promise.all([getRecipesVote(recipeData), getRecipesSaved(recipeData)]);
+        recipeData.forEach((recipe, i) => {
+          recipe.voted = voteData[i];
+          recipe.saved = savedData[i];
+        });
+      }
+      return recipeData;
+    },
+    [props.feedType]
+  );
 
   useEffect(() => {
     // do on feedtype change and initial render
     let listener = app.auth().onAuthStateChanged(async user => {
       listener();
-      let recipeData = await getRecipes(props.feedType);
-      let tagIds = {};
-      recipeData.forEach(recipe => Object.assign(tagIds, recipe.tagIds));
-      setTags(await getTags(tagIds));
-      if (user) {
-        let voteData = await getRecipesVote(recipeData);
-        recipeData.forEach((recipe, i) => (recipe.voted = voteData[i]));
-        let savedData = await getRecipesSaved(recipeData);
-        recipeData.forEach((recipe, i) => (recipe.saved = savedData[i]));
-      }
-      setRecipes(recipeData);
+      setTags(await getTags());
+      let recipeDataPage0 = await loadRecipes(user, 0);
+      setRecipes([recipeDataPage0]);
       setLoaded(true); // Forces component to re-render once recipe loading finished.
+      loadRecipes(user, 1).then(recipeDataPage1 => setRecipes([recipeDataPage0, recipeDataPage1]));
     });
     return () => {
       // return the cleanup function
@@ -68,7 +78,18 @@ const Feed = props => {
       setTags({});
       setLoaded(false);
     };
-  }, [props.feedType]);
+  }, [loadRecipes]);
+
+  useEffect(() => {
+    if (!recipes[page + 1]) {
+      loadRecipes(app.auth().currentUser, page + 1).then(nextPageRecipes => setRecipes([...recipes, nextPageRecipes]));
+    }
+    // we do not include recipes
+    // in the dependency array
+    // because that would cause
+    // an infinite rerender loop
+    // eslint-disable-next-line
+  }, [page, loadRecipes]);
 
   if (!loaded) {
     return (
@@ -82,20 +103,42 @@ const Feed = props => {
 
   return (
     <>
-      <CRow>
-        {!props.mapMode &&
-          recipes.length > 0 &&
-          recipes.map((recipe, idx) => (
-            <CCol xs="12" sm="6" md="4" key={idx}>
-              <RecipeCard recipe={recipe} tags={tags} setErrMsg={setErrMsg} />
-            </CCol>
-          ))}
-        {!props.mapMode && recipes.length === 0 && (
+      {!props.mapMode && recipes.length > 0 && (
+        <>
+          <CRow>
+            {recipes[page].map((recipe, idx) => (
+              <CCol xs="12" sm="6" md="4" key={idx}>
+                <RecipeCard recipe={recipe} tags={tags} setErrMsg={setErrMsg} />
+              </CCol>
+            ))}
+          </CRow>
+          <CRow style={{ marginBottom: 10 }}>
+            {recipes[page - 1] && (
+              <CCol>
+                <CButton color="info" onClick={() => setPage(page - 1)}>
+                  Prev Page
+                </CButton>
+              </CCol>
+            )}
+            {recipes[page + 1] && recipes[page + 1].length > 0 && (
+              <CCol className="d-flex justify-content-end">
+                <CButton color="info" onClick={() => setPage(page + 1)}>
+                  Next Page
+                </CButton>
+              </CCol>
+            )}
+          </CRow>
+        </>
+      )}
+      {!props.mapMode && recipes[page].length === 0 && (
+        <CRow>
           <CCard>
             <CCardBody>There seem to be no recipes here.</CCardBody>
           </CCard>
-        )}
-        {props.mapMode && (
+        </CRow>
+      )}
+      {props.mapMode && (
+        <CRow>
           <CCol xs="36" sm="18" md="12">
             <CCard>
               <CCardBody>
@@ -105,8 +148,8 @@ const Feed = props => {
               </CCardBody>
             </CCard>
           </CCol>
-        )}
-      </CRow>
+        </CRow>
+      )}
       <CModal show={errMsg !== ""} onClose={() => setErrMsg("")} color="danger" size="sm">
         <CModalHeader closeButton>
           <CModalTitle>ERROR!!</CModalTitle>
