5ac24fa61dc5abd56e1a4a0e18e527d38eba24cc
bpickard <pickbradley23@gmail.com>
2020-08-26 13:44:47 -0700

    Move Map to its own page separate from Feed

--- src/main/react/src/routes.js
+++ src/main/react/src/routes.js
@@ -3,14 +3,15 @@ import React from 'react';
 const Feed = React.lazy(() => import('./views/Feed'));
 const Recipe = React.lazy(() => import('./views/Recipe'));
 const AddRecipe = React.lazy(() => import('./views/AddRecipe'));
+const MapPage = React.lazy(() => import('./views/MapPage'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/popular', name: 'Popular', component: Feed, props: { feedType: 'popular', mapMode: false } },
-  { path: '/new', name: 'New', component: Feed, props: { feedType: 'new', mapMode: false } },
+  { path: '/popular', name: 'Popular', component: Feed, props: { feedType: 'popular' } },
+  { path: '/new', name: 'New', component: Feed, props: { feedType: 'new' } },
   { path: '/recipe', name: 'Recipe', component: Recipe },
   { path: '/addrecipe', name: 'Add Recipe', component: AddRecipe },
-  { path: '/map', name: 'Recipe Map', component: Feed, props: { feedType: 'popular', mapMode: true } },
+  { path: '/map', name: 'Recipe Map', component: MapPage, props: { feedType: 'popular' } },
 ];
 
 export default routes;
--- src/main/react/src/views/MapPage.js
+++ src/main/react/src/views/MapPage.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import {
   CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
   CCol,
+  CInput,
   CModal,
   CModalBody,
   CModalFooter,
@@ -10,10 +14,10 @@ import {
   CModalTitle,
   CRow,
 } from "@coreui/react";
-import RecipeCard from "../components/RecipeCard";
 import requestRoute, { getTags, getRecipesVote } from "../requests";
 import app from "firebase/app";
 import "firebase/auth";
+import FeedMap from "../components/FeedMap";
 
 const getRecipes = async () => {
   let res = await fetch(requestRoute + "api/post");
@@ -21,13 +25,16 @@ const getRecipes = async () => {
   return data;
 };
 
-const Feed = props => {
+const MapPage = props => {
   console.log(props.feedType);
   const [recipes, setRecipes] = useState([]);
   const [tags, setTags] = useState({});
   const [ready, setReady] = useState(false);
+  const [mapCenter, setMapCenter] = useState("");
 
   const [errMsg, setErrMsg] = useState("");
+
+  let searchBarVal = "";
 
   useEffect(() => {
     // do on first render
@@ -49,12 +56,32 @@ const Feed = props => {
   return (
     <>
       <CRow>
-        {ready &&
-          recipes.map((recipe, idx) => (
-            <CCol xs="12" sm="6" md="4" key={idx}>
-              <RecipeCard recipe={recipe} tags={tags} setErrMsg={setErrMsg} />
-            </CCol>
-          ))}
+        {ready && (
+          <CCol xs="36" sm="18" md="12">
+            <CCard>
+              <CCardHeader>
+                <CRow>
+                  <CCol>
+                    <CInput
+                      onChange={event => (searchBarVal = event.target.value)}
+                      placeholder="Search By Location"
+                    ></CInput>
+                  </CCol>
+                  <CCol>
+                    <CButton color="primary" onClick={() => setMapCenter(searchBarVal)}>
+                      Go
+                    </CButton>
+                  </CCol>
+                </CRow>
+              </CCardHeader>
+              <CCardBody>
+                <div className="min-vh-100">
+                  <FeedMap recipes={recipes} tags={tags} setErrMsg={setErrMsg} mapCenter={mapCenter} />
+                </div>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        )}
       </CRow>
       <CModal show={errMsg !== ""} onClose={() => setErrMsg("")} color="danger" size="sm">
         <CModalHeader closeButton>
@@ -71,8 +98,8 @@ const Feed = props => {
   );
 };
 
-Feed.propTypes = {
+MapPage.propTypes = {
   feedType: PropTypes.string,
 };
 
-export default Feed;
+export default MapPage;
